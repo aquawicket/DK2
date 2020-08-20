7bdd30d6bddd125b8188ef95d44056c24d506cf9
aquawicket2 <aquawicket@hotmail.com>
2020-08-19 21:39:09 -0500

    updated DKCMake

--- DKCMake/DKInit.cmake
+++ DKCMake/DKInit.cmake
@@ -1,5 +1,9 @@
 #### DK2.cmake ####
 
+MESSAGE("##########################")
+MESSAGE("###  DK2 Build System  ###")
+MESSAGE("##########################")
+
 IF(COMMAND cmake_policy)
 	CMAKE_POLICY(SET CMP0003 NEW) #https://cmake.org/cmake/help/v3.0/policy/CMP0003.html
 ENDIF(COMMAND cmake_policy)
